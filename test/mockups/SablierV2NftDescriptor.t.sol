120a28a4cedc6b6bb5e7ae22ad7f3f32c9aa78e0
andreivladbrg <andreivladbrg@gmail.com>
2023-03-15 15:34:05 +0200

    test: update the nft descriptor mockup

--- test/mockups/SablierV2NftDescriptor.t.sol
+++ test/mockups/SablierV2NftDescriptor.t.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19 <0.9.0;
 
-import { ISablierV2Lockup } from "@sablier/v2-core/interfaces/ISablierV2Lockup.sol";
-import { ISablierV2NftDescriptor } from "@sablier/v2-core/interfaces/ISablierV2NftDescriptor.sol";
+import { IERC721Metadata } from "@openzeppelin/token/ERC721/extensions/IERC721Metadata.sol";
+import { ISablierV2NFTDescriptor } from "@sablier/v2-core/interfaces/ISablierV2NFTDescriptor.sol";
 
-contract SablierV2NftDescriptor is ISablierV2NftDescriptor {
-    function tokenURI(ISablierV2Lockup lockup, uint256 streamId) external view override returns (string memory uri) {
-        lockup.getStartTime(streamId);
-        uri = "This is an NFT descriptor";
+contract SablierV2NftDescriptor is ISablierV2NFTDescriptor {
+    function tokenURI(
+        IERC721Metadata sablierContract,
+        uint256 streamId
+    ) external view override returns (string memory uri) {
+        streamId;
+        string memory symbol = sablierContract.symbol();
+        uri = string.concat("This is the NFT descriptor for ", symbol);
     }
 }
