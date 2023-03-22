9c41e3449a2b4a998e60b4bbb7eb612a897f57ee
andreivladbrg <andreivladbrg@gmail.com>
2023-03-22 16:57:35 +0200

    test: batchCreateWithMilestones function

---
+++ test/unit/batch-create-with-milestones/batchCreateWithMilestones.t.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.19 <0.9.0;
+
+import { Errors } from "src/libraries/Errors.sol";
+import { Batch } from "src/types/DataTypes.sol";
+
+import { Unit_Test } from "../Unit.t.sol";
+
+contract BatchCreateWithMilestones_Test is Unit_Test {
+    function setUp() public virtual override {
+        Unit_Test.setUp();
+
+        changePrank(users.sender);
+    }
+
+    /// @dev it should revert.
+    function test_RevertWhen_TotalAmountZero() external {
+        uint128 totalAmountZero = 0;
+        bytes memory data = abi.encodeCall(
+            target.batchCreateWithMilestones,
+            (dynamic, asset, totalAmountZero, defaultMilestonesParams(), defaultPermit2Params)
+        );
+        vm.expectRevert(abi.encodeWithSelector(Errors.SablierV2ProxyTarget_TotalAmountZero.selector));
+        proxy.execute(address(target), data);
+    }
+
+    modifier whenTotalAmountNotZero() {
+        _;
+    }
+
+    /// @dev it should revert.
+    function test_RevertWhen_ParamsCountZero() external whenTotalAmountNotZero {
+        Batch.CreateWithMilestones[] memory params;
+        bytes memory data = abi.encodeCall(
+            target.batchCreateWithMilestones, (dynamic, asset, DEFAULT_TOTAL_AMOUNT, params, defaultPermit2Params)
+        );
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Errors.SablierV2ProxyTarget_TotalAmountNotEqualToAmountsSum.selector, DEFAULT_TOTAL_AMOUNT, 0
+            )
+        );
+        proxy.execute(address(target), data);
+    }
+
+    modifier whenParamsCountNotZero() {
+        _;
+    }
+
+    /// @dev it should revert.
+    function test_RevertWhen_TotalAmountNotEqualToAmountsSum() external whenTotalAmountNotZero whenParamsCountNotZero {
+        uint128 totalAmount = DEFAULT_TOTAL_AMOUNT - 1;
+        bytes memory data = abi.encodeCall(
+            target.batchCreateWithMilestones,
+            (dynamic, asset, totalAmount, defaultMilestonesParams(), defaultPermit2Params)
+        );
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Errors.SablierV2ProxyTarget_TotalAmountNotEqualToAmountsSum.selector, totalAmount, DEFAULT_TOTAL_AMOUNT
+            )
+        );
+        proxy.execute(address(target), data);
+    }
+
+    modifier whenTotalAmountEqualToAmountsSum() {
+        _;
+    }
+
+    /// @dev it should create multiple streams.
+    function test_BatchCreateWithMilestones()
+        external
+        whenTotalAmountNotZero
+        whenParamsCountNotZero
+        whenTotalAmountEqualToAmountsSum
+    {
+        expectTransferFromCall(users.sender, address(proxy), DEFAULT_TOTAL_AMOUNT);
+        expectTransferFromCallMutiple(address(proxy), address(dynamic), DEFAULT_AMOUNT);
+
+        uint256[] memory streamIds = batchCreateWithMilestonesDefault();
+
+        assertEq(streamIds, DEFAULT_STREAM_IDS);
+    }
+}
