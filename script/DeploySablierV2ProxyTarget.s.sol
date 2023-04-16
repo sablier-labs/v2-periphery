27ec4a5d8c3cfc7dbd2d1f8ba324937414435bde
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-04-16 15:53:18 +0300

    refactor: delete superfluous "Script" inheritance

--- script/DeploySablierV2ProxyTarget.s.sol
+++ script/DeploySablierV2ProxyTarget.s.sol
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.8.19 <0.9.0;
 
-import { Script } from "forge-std/Script.sol";
-
 import { BaseScript } from "@sablier/v2-core/../script/shared/Base.s.sol";
 
 import { SablierV2ProxyTarget } from "src/SablierV2ProxyTarget.sol";
 
-contract DeploySablierV2ProxyTarget is Script, BaseScript {
+contract DeploySablierV2ProxyTarget is BaseScript {
     function run() public broadcaster returns (SablierV2ProxyTarget target) {
         target = new SablierV2ProxyTarget();
     }
