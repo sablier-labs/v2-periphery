15bd81f754745e5ff35a457d0570fc6efcd1a47d
andreivladbrg <andreivladbrg@gmail.com>
2023-05-01 17:00:10 +0300

    feat: add deployment scripts for proxy plugin

---
+++ script/DeployProxyPlugin.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.19 <0.9.0;
+
+import { BaseScript } from "@sablier/v2-core-script/shared/Base.s.sol";
+
+import { SablierV2ProxyPlugin } from "../src/SablierV2ProxyPlugin.sol";
+
+contract DeployProxyPlugin is BaseScript {
+    function run() public broadcaster returns (SablierV2ProxyPlugin plugin) {
+        plugin = new SablierV2ProxyPlugin();
+    }
+}
