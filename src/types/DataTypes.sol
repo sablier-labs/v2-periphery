9fd47df12c970a55318c48d44d7e95d71752e48f
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-05-12 23:35:05 +0300

    refactor: reorder struct fields

--- src/types/DataTypes.sol
+++ src/types/DataTypes.sol
@@ -13,44 +13,44 @@ library Batch {
 
     /// @notice Struct encapsulating all parameters of {SablierV2LockupDynamic.createWithDelta} except for the asset.
     struct CreateWithDeltas {
-        Broker broker;
-        address recipient;
-        LockupDynamic.SegmentWithDelta[] segments;
         address sender;
-        uint128 totalAmount;
         bool cancelable;
+        address recipient;
+        uint128 totalAmount;
+        Broker broker;
+        LockupDynamic.SegmentWithDelta[] segments;
     }
 
     /// @notice Struct encapsulating all parameters of {SablierV2LockupLinear.createWithDurations} except for the asset.
     struct CreateWithDurations {
-        Broker broker;
-        LockupLinear.Durations durations;
-        address recipient;
         address sender;
+        address recipient;
         uint128 totalAmount;
         bool cancelable;
+        LockupLinear.Durations durations;
+        Broker broker;
     }
 
     /// @notice Struct encapsulating all parameters of {SablierV2LockupDynamic.createWithMilestones} except for the
     /// asset.
     struct CreateWithMilestones {
-        Broker broker;
-        address recipient;
-        LockupDynamic.Segment[] segments;
         address sender;
-        uint128 totalAmount;
         uint40 startTime;
         bool cancelable;
+        address recipient;
+        uint128 totalAmount;
+        Broker broker;
+        LockupDynamic.Segment[] segments;
     }
 
     /// @notice Struct encapsulating all parameters of {SablierV2LockupLinear.createWithRange} except for the asset.
     struct CreateWithRange {
-        Broker broker;
-        LockupLinear.Range range;
-        address recipient;
         address sender;
+        address recipient;
         uint128 totalAmount;
         bool cancelable;
+        LockupLinear.Range range;
+        Broker broker;
     }
 }
 
