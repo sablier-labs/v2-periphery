780216862ce53405428fded290ccf6686f4440cb
andreivladbrg <andreivladbrg@gmail.com>
2023-05-26 15:40:32 +0300

    feat: add script to list an address on archive
    ci: add workflow to list an address on archive

---
+++ script/ListAddressOnArchive.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.19 <0.9.0;
+
+import { BaseScript } from "@sablier/v2-core-script/shared/Base.s.sol";
+
+import { ISablierV2Archive } from "../src/interfaces/ISablierV2Archive.sol";
+
+contract ListAddressOnArchive is BaseScript {
+    function run(ISablierV2Archive archive, address addr) public broadcaster {
+        archive.list(addr);
+    }
+}
