355f7d2f3cde67a79fe2e1834b9178c080187c20
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-05-31 21:10:17 +0300

    feat: add "DeployDeterministicPeriphery"
    fix: add "broadcaster" modifier in "DeployPeriphery"

---
+++ script/DeployDeterministicPeriphery.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.19 <=0.9.0;
+
+import { BaseScript } from "@sablier/v2-core-script/Base.s.sol";
+
+import { SablierV2Archive } from "../src/SablierV2Archive.sol";
+import { SablierV2ProxyPlugin } from "../src/SablierV2ProxyPlugin.sol";
+import { SablierV2ProxyTarget } from "../src/SablierV2ProxyTarget.sol";
+
+/// @notice Deploys all V2 Core contracts at deterministic addresses across chains, in the following order:
+///
+/// 1. {SablierV2Archive}
+/// 2. {SablierV2ProxyPlugin}
+/// 3. {SablierV2ProxyTarget}
+///
+/// @dev Reverts if any contract has already been deployed.
+contract DeployDeterministicPeriphery is BaseScript {
+    /// @dev The presence of the salt instructs Forge to deploy the contract via a deterministic CREATE2 factory.
+    /// https://github.com/Arachnid/deterministic-deployment-proxy
+    function run(address initialAdmin)
+        public
+        virtual
+        broadcaster
+        returns (SablierV2Archive archive, SablierV2ProxyPlugin plugin, SablierV2ProxyTarget target)
+    {
+        archive = new SablierV2Archive{ salt: ZERO_SALT }(initialAdmin);
+        plugin = new SablierV2ProxyPlugin{ salt: ZERO_SALT }(archive);
+        target = new SablierV2ProxyTarget{ salt: ZERO_SALT }();
+    }
+}
