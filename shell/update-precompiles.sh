3019891c401231c6d34f5f681bcc889163c1bf9c
andreivladbrg <andreivladbrg@gmail.com>
2023-08-10 18:06:06 +0300

    build: add target erc20 in update-precompiles

--- shell/update-precompiles.sh
+++ shell/update-precompiles.sh
@@ -14,7 +14,8 @@ FOUNDRY_PROFILE=optimized forge build
 # Retrieve the raw bytecodes, removing the "0x" prefix
 archive=$(cat out-optimized/SablierV2Archive.sol/SablierV2Archive.json | jq -r '.bytecode.object' | cut -c 3-)
 proxy_plugin=$(cat out-optimized/SablierV2ProxyPlugin.sol/SablierV2ProxyPlugin.json | jq -r '.bytecode.object' | cut -c 3-)
-proxy_target=$(cat out-optimized/SablierV2ProxyTarget.sol/SablierV2ProxyTarget.json | jq -r '.bytecode.object' | cut -c 3-)
+proxy_target_erc20=$(cat out-optimized/SablierV2ProxyTargetERC20.sol/SablierV2ProxyTargetERC20.json | jq -r '.bytecode.object' | cut -c 3-)
+proxy_target_permit2=$(cat out-optimized/SablierV2ProxyTargetPermit2.sol/SablierV2ProxyTargetPermit2.json | jq -r '.bytecode.object' | cut -c 3-)
 
 precompiles_path="test/utils/Precompiles.sol"
 if [ ! -f $precompiles_path ]; then
@@ -25,7 +26,8 @@ fi
 # Replace the current bytecodes
 sd "(BYTECODE_ARCHIVE =)[^;]+;" "\$1 hex\"$archive\";" $precompiles_path
 sd "(BYTECODE_PROXY_PLUGIN =)[^;]+;" "\$1 hex\"$proxy_plugin\";" $precompiles_path
-sd "(BYTECODE_PROXY_TARGET =)[^;]+;" "\$1 hex\"$proxy_target\";" $precompiles_path
+sd "(BYTECODE_PROXY_TARGET_ERC20 =)[^;]+;" "\$1 hex\"$proxy_target_erc20\";" $precompiles_path
+sd "(BYTECODE_PROXY_TARGET_PERMIT2 =)[^;]+;" "\$1 hex\"$proxy_target_permit2\";" $precompiles_path
 
 # Reformat the code with Forge
 forge fmt $precompiles_path
