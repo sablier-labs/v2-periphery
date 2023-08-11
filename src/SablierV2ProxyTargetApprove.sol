58b59f4340b32e739204556be70fddd5b0d1cf0a
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-08-11 14:26:40 +0300

    fix: use "safeTransferFrom"

--- src/SablierV2ProxyTargetApprove.sol
+++ src/SablierV2ProxyTargetApprove.sol
@@ -2,12 +2,15 @@
 pragma solidity >=0.8.19;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import { SablierV2ProxyTarget } from "./abstracts/SablierV2ProxyTarget.sol";
 
 /// @title SablierV2ProxyTargetApprove
 /// @notice Proxy target contract that implements the transfer logic using standard ERC-20 approvals.
 contract SablierV2ProxyTargetApprove is SablierV2ProxyTarget {
+    using SafeERC20 for IERC20;
+
     /// @notice Transfers the given `amount` of `asset` to the Sablier contract using standard the ERC-20
     /// approve and transfer flow, and then approves Sablier to spend the funds.
     /// @dev The `transferData` data is ignored in this implementation.
@@ -24,7 +27,7 @@ contract SablierV2ProxyTargetApprove is SablierV2ProxyTarget {
         address owner = _getOwner();
 
         // Transfer funds from the proxy owner to the proxy.
-        asset.transferFrom({ from: owner, to: address(this), amount: amount });
+        asset.safeTransferFrom({ from: owner, to: address(this), value: amount });
 
         // Approve the Sablier contract to spend funds.
         _approve(sablierContract, asset, amount);
