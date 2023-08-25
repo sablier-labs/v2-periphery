223437e65fbec1cf80ce8f440ad459e6643d1470
andreivladbrg <andreivladbrg@gmail.com>
2023-08-25 14:24:02 +0300

    refactor: remove SafeERC20 from target push

--- src/SablierV2ProxyTargetPush.sol
+++ src/SablierV2ProxyTargetPush.sol
@@ -2,15 +2,12 @@
 pragma solidity >=0.8.19;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import { SablierV2ProxyTarget } from "./abstracts/SablierV2ProxyTarget.sol";
 
 /// @title SablierV2ProxyTargetPush
 /// @notice Proxy target contract that implements a push-based model for transferring funds.
 contract SablierV2ProxyTargetPush is SablierV2ProxyTarget {
-    using SafeERC20 for IERC20;
-
     /// @notice The name of this function is a bit misleading, as it does not actually transfer assets to the proxy. That
     /// is left for the user to do. Instead, this function only approves the Sablier contract to spend the funds. Still,
     /// the name is retained so that the logic defined in {SablierV2ProxyTarget} can be reused.
