eacaa4998efe981423a5ea4097e4401b534de539
andreivladbrg <andreivladbrg@gmail.com>
2023-09-20 14:04:24 +0300

    refactor: change order of parameters in salt

--- src/SablierV2MerkleStreamerFactory.sol
+++ src/SablierV2MerkleStreamerFactory.sol
@@ -55,7 +55,7 @@ contract SablierV2MerkleStreamerFactory is ISablierV2MerkleStreamerFactory {
         returns (ISablierV2MerkleStreamerLL merkleStreamerLL)
     {
         // Hash the parameters to generate a salt.
-        bytes32 salt = keccak256(abi.encodePacked(lockupLinear, initialAdmin, asset, merkleRoot, expiration));
+        bytes32 salt = keccak256(abi.encodePacked(initialAdmin, lockupLinear, asset, merkleRoot, expiration));
 
         // Deploy the Merkle streamer with CREATE2.
         merkleStreamerLL = new SablierV2MerkleStreamerLL{salt: salt} (
