7f868bfe0ea69dfb79e48a92e3d925661deb5a1c
andreivladbrg <andreivladbrg@gmail.com>
2024-01-06 15:14:06 +0200

    chore: use pragma solidity >=0.8.22

--- script/DeployProtocol.s.sol
+++ script/DeployProtocol.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity >=0.8.19 <0.9.0;
+pragma solidity >=0.8.22 <0.9.0;
 
 import { SablierV2Comptroller } from "@sablier/v2-core/src/SablierV2Comptroller.sol";
 import { SablierV2LockupDynamic } from "@sablier/v2-core/src/SablierV2LockupDynamic.sol";
