32cbe90075434d1c48d634edb2303fce5b9c6834
andreivladbrg <andreivladbrg@gmail.com>
2024-02-05 15:11:49 +0200

    feat: add a util function to construct the create2 salt

--- script/DeployDeterministicBatch.s.sol
+++ script/DeployDeterministicBatch.s.sol
@@ -8,9 +8,8 @@ import { SablierV2Batch } from "../src/SablierV2Batch.sol";
 /// @notice Deploys {SablierV2Batch} at a deterministic address across chains.
 /// @dev Reverts if the contract has already been deployed.
 contract DeployDeterministicBatch is BaseScript {
-    /// @dev The presence of the salt instructs Forge to deploy contracts via this deterministic CREATE2 factory:
-    /// https://github.com/Arachnid/deterministic-deployment-proxy
     function run(string memory create2Salt) public virtual broadcast returns (SablierV2Batch batch) {
-        batch = new SablierV2Batch{ salt: bytes32(abi.encodePacked(create2Salt)) }();
+        bytes32 salt = bytes32(abi.encodePacked(create2Salt));
+        batch = new SablierV2Batch{ salt: salt }();
     }
 }
