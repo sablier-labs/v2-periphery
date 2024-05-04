1055c00e5e377a56a0a27854dd6a12caf5bdf894
andreivladbrg <andreivladbrg@gmail.com>
2024-05-04 20:14:45 +0300

    Make the project compatible with Hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,100 @@
+import * as dotenv from "dotenv";
+
+import "@nomicfoundation/hardhat-foundry";
+import "@matterlabs/hardhat-zksync";
+import "@nomiclabs/hardhat-solhint";
+import "@typechain/hardhat";
+import fs from "fs";
+import "hardhat-preprocessor";
+import { HardhatUserConfig } from "hardhat/config";
+
+dotenv.config();
+
+let deployPrivateKey = process.env.PV_KEY as string;
+if (!deployPrivateKey) {
+  // default first account deterministically created by local nodes like `npx hardhat node` or `anvil`
+  throw "No deployer private key set in .env";
+}
+
+/**
+ * Reads the remappings.txt file and returns an array of arrays.
+ * @returns {string[][]}
+ */
+function getRemappings(): string[][] {
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean) // remove empty lines
+    .map((line) => line.trim().split("="));
+}
+
+/**
+ * Generates hardhat network configuration
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.23",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+      viaIR: true,
+    },
+    // @ts-ignore
+  },
+  networks: {
+    zkSyncTestnet: {
+      url: "https://sepolia.era.zksync.dev",
+      ethNetwork: "sepolia",
+      zksync: true,
+      verifyURL: "https://explorer.sepolia.era.zksync.dev/contract_verification",
+      chainId: 300,
+    },
+    zkSyncMainnet: {
+      url: "https://mainnet.era.zksync.io",
+      ethNetwork: "mainnet",
+      zksync: true,
+      verifyURL: "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
+      chainId: 324,
+    },
+    inMemoryNode: {
+      url: "http://127.0.0.1:8011",
+      ethNetwork: "localhost", // in-memory node doesn't support eth node; removing this line will cause an error
+      zksync: true,
+      chainId: 260,
+    },
+  },
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line: string) => {
+        if (line.match(/^\s*import /i)) {
+          for (const [from, to] of getRemappings()) {
+            if (line.includes(from)) {
+              line = line.replace(from, to);
+              break;
+            }
+          }
+        }
+        return line;
+      },
+    }),
+  },
+  paths: {
+    sources: "./contracts",
+    cache: "./cache_hardhat",
+  },
+  zksolc: {
+    version: "latest",
+    settings: {
+      optimizer: {
+        enabled: true,
+        mode: "z",
+        fallback_to_optimizing_for_size: true,
+      },
+    },
+  },
+};
+
+export default config;
