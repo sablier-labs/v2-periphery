578ab116f9619262a4d8757d1c702de8af7edf48
smol-ninja <shubhamy2015@gmail.com>
2024-06-20 16:06:43 +0100

    feat: isValidMerkleLT

---
+++ test/integration/merkle-lockup/factory/is-valid-merkle-lt/isValidMerkleLT.t.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.22 <0.9.0;
+
+import { MAX_UD2x18, ud2x18 } from "@prb/math/src/UD2x18.sol";
+
+import { MerkleLT } from "src/types/DataTypes.sol";
+
+import { MerkleLockup_Integration_Test } from "../../MerkleLockup.t.sol";
+
+contract IsValidMerkleLT_Integration_Test is MerkleLockup_Integration_Test {
+    function test_RevertWhen_SumOverflow() public {
+        MerkleLT.TrancheWithPercentage[] memory tranches = defaults.tranchesWithPercentages();
+        tranches[0].unlockPercentage = MAX_UD2x18;
+
+        vm.expectRevert();
+        merkleLockupFactory.isValidMerkleLT(tranches);
+    }
+
+    modifier whenSumDoesNotOverflow() {
+        _;
+    }
+
+    modifier whenTotalPercentageNotOneHundred() {
+        _;
+    }
+
+    function test_TotalPercentageLessThanOneHundred()
+        public
+        view
+        whenSumDoesNotOverflow
+        whenTotalPercentageNotOneHundred
+    {
+        MerkleLT.TrancheWithPercentage[] memory tranchesWithPercentages = defaults.tranchesWithPercentages();
+        tranchesWithPercentages[0].unlockPercentage = ud2x18(0.05e18);
+        tranchesWithPercentages[1].unlockPercentage = ud2x18(0.2e18);
+
+        assertFalse(merkleLockupFactory.isValidMerkleLT(tranchesWithPercentages), "isValidMerkleLT");
+    }
+
+    function test_TotalPercentageGreaterThanOneHundred()
+        public
+        view
+        whenSumDoesNotOverflow
+        whenTotalPercentageNotOneHundred
+    {
+        MerkleLT.TrancheWithPercentage[] memory tranchesWithPercentages = defaults.tranchesWithPercentages();
+        tranchesWithPercentages[0].unlockPercentage = ud2x18(0.5e18);
+        tranchesWithPercentages[1].unlockPercentage = ud2x18(0.6e18);
+
+        assertFalse(merkleLockupFactory.isValidMerkleLT(tranchesWithPercentages), "isValidMerkleLT");
+    }
+
+    modifier whenTotalPercentageOneHundred() {
+        _;
+    }
+
+    function test_IsValidMerkleLT() public view whenSumDoesNotOverflow whenTotalPercentageOneHundred {
+        assertTrue(merkleLockupFactory.isValidMerkleLT(defaults.tranchesWithPercentages()), "isValidMerkleLT");
+    }
+}
