5304f8129e9b770edcc87bb4b19bac38a3522c5a
smol-ninja <shubhamy2015@gmail.com>
2024-07-24 14:23:06 +0100

    refactor: remove redundant imports

--- src/types/DataTypes.sol
+++ src/types/DataTypes.sol
@@ -3,7 +3,6 @@ pragma solidity >=0.8.22;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { UD2x18 } from "@prb/math/src/UD2x18.sol";
-import { ISablierV2Lockup } from "@sablier/v2-core/src/interfaces/ISablierV2Lockup.sol";
 import { Broker, LockupDynamic, LockupLinear, LockupTranched } from "@sablier/v2-core/src/types/DataTypes.sol";
 
 library BatchLockup {
