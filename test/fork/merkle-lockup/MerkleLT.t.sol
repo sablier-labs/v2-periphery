3d9d8df4a560f09e066fd13794dfa76f8b18579a
smol-ninja <shubhamy2015@gmail.com>
2024-08-01 16:08:03 +0100

    test(fork): fix claim event assertion

--- test/fork/merkle-lockup/MerkleLT.t.sol
+++ test/fork/merkle-lockup/MerkleLT.t.sol
@@ -144,6 +144,8 @@ abstract contract MerkleLT_Fork_Test is Fork_Test {
         vars.leafPos = Arrays.findUpperBound(leaves, vars.leafToClaim);
 
         vars.expectedStreamId = lockupTranched.nextStreamId();
+
+        vm.expectEmit({ emitter: address(vars.merkleLT) });
         emit Claim(
             vars.indexes[params.posBeforeSort],
             vars.recipients[params.posBeforeSort],
